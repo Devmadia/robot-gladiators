de4f8f31e4eee1ffb1709a6a55a8a32b59b5ff95
Devmadia <agathaphan@icloud.com>
2020-06-01 23:02:52 -0500

    updated to perform attacks, calculate health reduction for Player's robot and enemy robot.

--- assets/js/game.js
+++ assets/js/game.js
@@ -9,6 +9,7 @@ var enemyName = "Roborto";
 var enemyHealth = 50;
 var enemyAttack = 12;
 
+// create function
 var fight = function() {
     // Alert users that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
@@ -20,10 +21,15 @@ var fight = function() {
     console.log(
         playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
       );
-  
-    // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
-  
-    // Log a resulting message to the console so we know that it worked.
 
-    
+    // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+    playerHealth = playerHealth - enemyAttack;
+
+    // Log a resulting message to the console so we know that it worked.
+    console.log(
+        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+      );
   };
+
+// execute function
+fight();
