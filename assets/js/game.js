af611183aaf4c4c176be8b898e1f627f661d16f1
Devmadia <agathaphan@icloud.com>
2020-06-01 21:42:29 -0500

    updated comments to reflect current working code

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,7 +1,8 @@
-// this creates a function named "fight"
 
+// stores player's robot name under variable playerName
 var playerName = window.prompt("What is your robot's name?");
 
+// this creates a function named "fight"
 function fight() {
     window.alert("The fight has begun!");
   }
