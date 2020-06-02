26c01d6727201383d5b09252111aaaf69dfa324b
Devmadia <agathaphan@icloud.com>
2020-06-01 21:40:12 -0500

    revised window prompt with preceding variable condition to set robot name to playerName

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,6 @@
 // this creates a function named "fight"
 
-window.prompt("What is your robot's name?");
+var playerName = window.prompt("What is your robot's name?");
 
 function fight() {
     window.alert("The fight has begun!");
